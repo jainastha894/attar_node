162fceee806b8c30340ed624336161972462cd42
Akshayj2005 <bbpsgrakshayj2005@gmail.com>
2026-01-05 22:40:34 +0530

    feat: Add favicon support and update icon links across multiple views

--- src/app.js
+++ src/app.js
@@ -58,6 +58,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // Parse JSON request bodies
 
+
 // Routes
 app.use("/", pageRoutes);
 app.use(adminRoutes);
