4573ec5a12f01b69a4dd75ec2ddb60deedf3e557
ShekharKundra <shekhar.kundra@gmail.com>
2026-01-08 15:42:30 +0530

    final code

--- src/app.js
+++ src/app.js
@@ -17,13 +17,16 @@ const PORT = process.env.PORT || 3000;
 
 
 
-try {
-  await mongoose.connect(process.env.MONGOURL);
-  console.log("MongoDB connected");
-} catch (err) {
-  console.error("MongoDB connection failed:", err.message);
-  process.exit(1);
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGOURL);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err.message);
+    process.exit(1);
+  }
+};
+connectDB();
 
 //session config
 app.use(
